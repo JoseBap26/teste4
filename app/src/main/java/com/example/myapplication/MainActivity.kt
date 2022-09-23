06de349ac911c69e7646dcf7b8d53d45b2d5b3d5
Jose <zebaptista26@gmail.com>
2022-09-23 09:55:03 +0100

    segundo commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     //test
+    //teste2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
